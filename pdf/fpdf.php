c9e7a4400cf105dbf8efab81c2495346bef6a841
JITHESH K <63106428+jithesh4@users.noreply.github.com>
2022-04-22 16:49:23 +0530

    Update fpdf.php

--- pdf/fpdf.php
+++ pdf/fpdf.php
@@ -11,7 +11,7 @@ namespace Jithesh\Fpdf;
 
 define('FPDF_VERSION','1.82');
 
-class FPDF
+class Fpdf
 {
 protected $page;               // current page number
 protected $n;                  // current object number
